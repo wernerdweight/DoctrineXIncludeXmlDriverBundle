2045ff7c3a6daf22d9d846c040e1bb76f0eca763
Werner Dweight <wd@wernerdweight.com>
2019-08-01 16:38:11 +0200

    🦷 Initial commit

---
+++ src/DoctrineXIncludeXmlDriverBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace WernerDweight\DoctrineXIncludeXmlDriverBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoctrineXIncludeXmlDriverBundle extends Bundle
+{
+}
